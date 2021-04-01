5e4ccf243ad78842080dc6c107d5f71f0bc1851a
Hifz ur Rehman Ali <aliijaz2010@yahoo.com>
2021-04-01 15:44:47 +0500

    Deployed

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -4,34 +4,83 @@ import * as targets from "@aws-cdk/aws-events-targets";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as appsync from "@aws-cdk/aws-appsync";
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
-// import * as cloudfront from '@aws-cdk/aws-cloudfront';
-// import * as origins from '@aws-cdk/aws-cloudfront-origins';
-// import * as s3 from '@aws-cdk/aws-s3';
-// import * as s3deploy from '@aws-cdk/aws-s3-deployment';
+// import * as cognito from "@aws-cdk/aws-cognito"
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3deploy from '@aws-cdk/aws-s3-deployment';
 export class BackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
-    // const myBucket = new s3.Bucket(this, 'Todobucket');
+    const myBucket = new s3.Bucket(this, 'Bookmarkbucket');
 
-    // const CloudFront = new cloudfront.Distribution(this, 'Cloudfront-dist-for-todo', {
-    //   defaultBehavior: { origin: new origins.S3Origin(myBucket) },
-    //   defaultRootObject: "index.html"
+    const CloudFront = new cloudfront.Distribution(this, 'Cloudfront-dist-for-Bookmark', {
+      defaultBehavior: { origin: new origins.S3Origin(myBucket) },
+      defaultRootObject: "index.html"
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployBookmarkEvent', {
+      sources: [s3deploy.Source.asset('../frontend-bookmark/public')],
+      destinationBucket: myBucket,
+      distribution: CloudFront,
+      distributionPaths: ["/*"]
+    });
+
+    new cdk.CfnOutput(this, "bookmarkdomain", {
+      value: CloudFront.domainName,
+    });
+    // const userPool = new cognito.UserPool(this, "myuserpool", {
+    //   selfSignUpEnabled: true,
+    //   userVerification: {
+    //     emailSubject: "Verify your email for our awesome app!",
+    //     emailBody:
+    //       "Hello, Thanks for signing up to our awesome app! Your verification code is {####}",
+    //     emailStyle: cognito.VerificationEmailStyle.CODE,
+    //   },
+    //   signInAliases: {
+    //     username: true,
+    //     email: true,
+    //   },
+    //   autoVerify: { email: true },
+    //   signInCaseSensitive: false,
+    //   standardAttributes: {
+    //     fullname: {
+    //       required: true,
+    //       mutable: true,
+    //     },
+    //     email: {
+    //       required: true,
+    //       mutable: false,
+    //     },
+    //   },
+    //   accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
     // });
 
-    // new s3deploy.BucketDeployment(this, 'DeployTodoEvent', {
-    //   sources: [s3deploy.Source.asset('. ./front-end/public')],
-    //   destinationBucket: myBucket,
-    //   distribution: CloudFront,
-    //   distributionPaths: ["/*"]
+    // const client = new cognito.UserPoolClient(this, "app-client", {
+    //   userPool: userPool,
+    //   generateSecret: true,
+    //   oAuth: {
+    //     flows: {
+    //       authorizationCodeGrant: true,
+    //     },
+    //     scopes: [cognito.OAuthScope.OPENID, cognito.OAuthScope.EMAIL],
+    //     callbackUrls: [`http://localhost:8000/dashboard`],
+    //     logoutUrls: [`http://localhost:8000`],
+    //   },
     // });
 
-    // new cdk.CfnOutput(this, "bookmarkdomain", {
-    //   value: CloudFront.domainName,
+    // const domain = userPool.addDomain("CognitoDomain", {
+    //   cognitoDomain: {
+    //     domainPrefix: "my-bookmark-app",
+    //   },
     // });
 
+    // const signInUrl = domain.signInUrl(client, {
+    //   redirectUri: `http://localhost:8000/dashboard`, // must be a URL configured under 'callbackUrls' with the client
+    // });
     const api = new appsync.GraphqlApi(this, "BookmarkeventAppsync", {
       name: "BookmarkeventAppsync",
       schema: appsync.Schema.fromAsset("graphql/schema.gql"),
